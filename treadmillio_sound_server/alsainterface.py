d57d0eeb431a33dccfd2da4833569a65dd7e3531
Rice University Neuroengineering Lab <caleb.kemere@rice.edu>
2022-07-13 17:04:56 -0500

    switch back from nonblocking to normal

--- treadmillio_sound_server/alsainterface.py
+++ treadmillio_sound_server/alsainterface.py
@@ -152,7 +152,7 @@ class ALSAPlaybackSystem():
 
         # Open alsa device
         self.adevice = alsaaudio.PCM(type=alsaaudio.PCM_PLAYBACK, 
-            mode=alsaaudio.PCM_NONBLOCK, # PCM_NORMAL, 
+            mode=alsaaudio.PCM_NORMAL, # PCM_NORMAL, 
             rate=fs, channels=num_channels, format=alsaaudio.PCM_FORMAT_S16_LE, 
             periodsize=buffer_size, 
             device=device)
